823c7d857f0c9dd3c8dd6efd0b43bf9a5130d63f
Alexandre Levesque <alevek.75@gmail.com>
2020-09-29 21:32:46 +0200

    yelp routing done miss html

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  septime = Restaurant.create(name: 'Septime', address:'Paris 11ème', phone_number: '01 40 21 08 50', category:'french')
+  mokonuts = Restaurant.create(name: 'Mokonuts', address:'Paris 11ème', phone_number: '01 40 08 21 50', category:'japanese')
+  les_arlots = Restaurant.create(name: 'Les Arlots', address:'Yvelines', phone_number: '01 91 50 08 21', category:'belgian')
+  passerini = Restaurant.create(name: 'Passerini', address:'Paris 12ème', phone_number: '01 56 21 50 43', category:'italian')
+  double_dragon = Restaurant.create(name: 'Double Dragon', address:'Paris 10ème', phone_number: '01 40 76 53 23', category:'chinese')
